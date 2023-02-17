5c2391e544d52773ec0e9569f207f9d1e4151a92
seyprince <princesey@outlook.com>
2023-02-17 03:13:18 +0000

    printing the size of comp types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - printing out the size of computer types
+* Return: 0
+*/
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
+
