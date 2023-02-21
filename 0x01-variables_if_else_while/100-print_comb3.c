de2d35cf29792e282025393946ba3091f08c30aa
seyprince <princesey@outlook.com>
2023-02-21 00:46:37 +0000

    print combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - printing double number combinations
+ * Return: 0
+ */
+int main(void)
+{
+	int f, s;
+
+	for (f = 0; f <= 10; f++)
+	{
+		for (s = 0; s <= 10; s++)
+		{
+			if (f * 10 + s < s * 10 + f)
+			{
+			putchar(f + '0');
+			putchar(s + '0');
+			if (f != 8 && s != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
