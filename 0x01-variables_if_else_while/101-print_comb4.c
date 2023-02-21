b16667782f2dbce85482670e1325de4e0476b296
seyprince <princesey@outlook.com>
2023-02-21 01:18:33 +0000

    print combination 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - a combination of three digits
+ * Return: 0
+ */
+int main(void)
+{
+	int f, s, t;
+
+	for (f = 0 ; f < 10 ; f++)
+	{
+		for (s = 0 ; s < 10 ; s++)
+		{
+			for (t = 0 ; t < 10 ; t++)
+				if (!(t == s) || (s == f) || (s > t) || (f > s))
+				{
+					putchar(f + '0');
+					putchar(s + '0');
+					putchar(t + '0');
+					if (!(t == 9 && s == 8 && f == 7))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
