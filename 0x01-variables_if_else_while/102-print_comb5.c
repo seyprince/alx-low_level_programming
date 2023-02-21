b8f3df5b555ebad78a62b2515badf5d614e11252
seyprince <princesey@outlook.com>
2023-02-21 01:40:13 +0000

    combination 5 from 0 to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - combination fro 0 - 99
+ * Return: 0
+ */
+int main(void)
+{
+	int f, s;
+
+	for (f = 0; f < 100; f++)
+	{
+		for (s = 0; s < 100; s++)
+		{
+			if (f < s)
+			{
+				putchar((f / 10) + 48);
+				putchar((f % 10) + 48);
+				putchar(' ');
+				putchar((s / 10) + 48);
+				putchar((s % 10) + 48);
+				if (f != 98 && s != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
