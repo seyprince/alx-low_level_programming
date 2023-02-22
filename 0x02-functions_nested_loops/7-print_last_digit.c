e441ac1f3eb5b97c9eb82381e7d80a2e738e8890
seyprince <princesey@outlook.com>
2023-02-22 04:52:05 +0000

    print last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#includ "main.h"
+/**
+ * print_the_last_digit - prints the last digit of a number
+ * @n: the number inputed
+ * Return: valure of last digit
+ */
+int print_the_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		l = l * -1;
+	}
+	_putchar(l + '0');
+	return (l);
+}
