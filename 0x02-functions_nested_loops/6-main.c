e38590087ba68e82a5409fb533ca318ab897777d
seyprince <princesey@outlook.com>
2023-02-22 04:36:20 +0000

    absolute value

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -9,13 +9,13 @@ int main(void)
 
     int r;
 
-    r = _abs(-1);
+    r = abs(-1);
     printf("%d\n", r);
-    r = _abs(0);
+    r = abs(0);
     printf("%d\n", r);
-    r = _abs(1);
+    r = abs(1);
     printf("%d\n", r);
-    r = _abs(-98);
+    r = abs(-98);
     printf("%d\n", r);
     return (0);
 }
