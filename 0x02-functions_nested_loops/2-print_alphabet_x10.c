13882b7bd1d6bccd6b3250fed8492ee86af7d048
seyprince <princesey@outlook.com>
2023-02-22 03:21:21 +0000

    printing alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char al;
+
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			_putchar(al);
+		}
+		_putchat('\n');
+		i++;
+	}
+}
