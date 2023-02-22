df12052d637b8edefbc776facc3df6191615f7ca
seyprince <princesey@outlook.com>
2023-02-22 03:56:55 +0000

    sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - sign of a number
+ * @n: number used
+ * Return: 1 for positive -1 for negative 0 for anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		putchar(48);
+		return (0);
+	}
+}
