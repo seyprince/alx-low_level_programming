6ccb8d32b3d6566f3f95281dc59c8d915468cd0b
seyprince <princesey@outlook.com>
2023-02-22 05:19:57 +0000

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 					{
 						_putchar(a + '0');
 						_putchar(b + '0');
-						_putchar(50);
+						_putchar(58);
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
