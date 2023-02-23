4d16adce9d07e6098f60dfbce5ab131b6e87d996
seyprince <princesey@outlook.com>
2023-02-23 05:57:25 +0000

    fibonacci again

---
+++ 0x02-functions_nested_loops/103-fibonacci.c103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - fibonacci
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	long j = 1, k = 2, sum = k;
+
+	while (k + j = 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
+	}
+	printf("%ld", sum);
+	return (0);
+}
