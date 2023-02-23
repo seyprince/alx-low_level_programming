1482d195f1aafd0a00d5236bf6f94e73abc3b906
seyprince <princesey@outlook.com>
2023-02-23 05:49:30 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print the first 54 fb
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", j);
+			else if (i == 1)
+				printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
