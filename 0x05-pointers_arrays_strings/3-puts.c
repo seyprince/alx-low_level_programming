71065bf43a776bdf423ff0a556ed099b65269abb
seyprince <princesey@outlook.com>
2023-02-28 02:21:04 +0000

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _puts - printing a string
+ * @str - string to be printed
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
