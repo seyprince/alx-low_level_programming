19dd380d8c58b9e1ca4e073744e45c87138b812b
seyprince <princesey@outlook.com>
2023-03-07 01:08:55 +0000

    length of prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - entry
+ * @s: input
+ * @accept: input
+ * Return: 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned input n = 0;
+
+	int r;
+
+	while (*s)
+	{
+		for ( r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
