8b5502f4f1f3d3694a5989f85981f35523acace2
seyprince <princesey@outlook.com>
2023-03-07 00:21:12 +0000

    memory set up

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * _memset - memory
+ * @s: starting
+ * @b: byte
+ * @n: size
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
