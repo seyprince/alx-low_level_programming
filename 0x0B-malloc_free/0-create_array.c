a34bf5f5293ce5d8a63eb0a3378e3604b769f833
seyprince <princesey@outlook.com>
2023-03-14 11:47:28 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = malloc(size * sizeof(char));
-
+	array = malloc(size * sizeof(char));
 	if (array == NULL)
 	{
 		return (NULL);
