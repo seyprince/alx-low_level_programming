d30f7543c2d0bf948f81b3c49efcfa2c96b53144
seyprince <princesey@outlook.com>
2023-03-15 01:04:50 +0000

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,16 +7,19 @@
  */
 char *argstostr(int ac, char **av)
 {
+	int total_length = 0;
+
+	int i, j, k;
+
+	char *result;
+
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	int total_length = 0;
-
-	for (int i = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		int j = 0;
-
+		j = 0;
 		while (av[i][j] != '\0')
 		{
 			total_length++;
@@ -24,18 +27,15 @@ char *argstostr(int ac, char **av)
 		}
 		total_length++;
 	}
-	char *result = malloc((total_length + 1) * sizeof(char));
-
+	result = malloc((total_length + 1) * sizeof(char));
 	if (result == NULL)
 	{
 		return (NULL);
 	}
-	int k = 0;
-
-	for (int i = 0; i < ac; i++)
+	k = 0;
+	for (i = 0; i < ac; i++)
 	{
-		int j = 0;
-
+		j = 0;
 		while (av[i][j] != '\0')
 		{
 			result[k] = av[i][j];
@@ -48,3 +48,4 @@ char *argstostr(int ac, char **av)
 	result[k] = '\0';
 	return (result);
 }
+
