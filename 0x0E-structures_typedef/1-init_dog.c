1962d8aee6762490fa90b8b5ef97ff6936ddfd62
seyprince <princesey@outlook.com>
2023-03-20 20:54:59 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,8 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d.name = "spooky";
-	d.age = "45";
-	d.owner = "madam";
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
 }
 
