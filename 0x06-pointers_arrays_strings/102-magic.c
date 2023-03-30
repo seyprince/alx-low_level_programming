c8e6729e91ece584d91886be6d15e787172abd69
seyprince <princesey@outlook.com>
2023-03-30 19:46:54 +0000

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - magic file
+ * Return: 0
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
+	a[2] = 1024;
+	p = &n;
 /*
  * write your line of code here...
  * Remember:
