8fafcdf93881974000194bb4f93d3b5e270dae01
seyprince <princesey@outlook.com>
2023-04-03 11:30:24 +0000

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Compile all .c files into object files
-gcc -c *.c
-
-# Create the static library using ar command
-ar -rc liball.a *.o
-
-# Index the library for faster linking
-ranlib liball.a
-
-# Clean up by removing object files
-rm *.o
-
+gcc -c *.c && ar rcs liball.a *.o
