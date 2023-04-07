e6baa26f530a10b1d7e1986182c7159d8aa10587
seyprince <princesey@outlook.com>
2023-04-07 18:53:43 +0000

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_listint - prints a linked lists
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		printf("The linked list is empty.\n");
+		return (count);
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
