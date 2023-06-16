674a0dbc757229f66ed672f3f31037c8da9e4c4b
seyprince <princesey@outlook.com>
2023-06-16 09:16:25 +0000

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function
+ * @h: pointer
+ * Return: count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t a;
+
+	for (a = 0; h != NULL; a++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (a);
+}
